39132041e2b55b237d03f986d5bec6a885e97ef3
Кузнецов Андрей Андреевич (53506/11) <andrew.a.kuznecov@gmail.com>
2013-11-01 22:13:17 +0400

    homework_3
    Сделано с дополнительными заданиями

---
+++ practice_3/project/library/models.py
@@ -0,0 +1,46 @@
+# ~*~ coding: utf-8 ~*~
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+'''
+Author
+'''
+class Author(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name  = models.CharField(max_length=32)
+    email = models.EmailField(blank=True,null=True)
+    def get_absolute_url(self):
+        return "/library/authors/%i" % self.id
+    def __unicode__(self):
+        return u'%s %s.' % (self.first_name, self.last_name)
+#IntegrityError: Problem installing fixture 'data.json': Could not load library.Author(pk=3): library_author.email may not be NULL
+'''
+Book
+'''
+class Book(models.Model):
+    title = models.CharField('Название',max_length=128)
+    authors = models.ManyToManyField('Author')
+    publisher = models.ForeignKey('Publisher')
+    publication_date = models.DateField(auto_now_add=True)
+    # функция необходима для того, чтобы при выводе объекта Post
+    # как строки выводился вместо этого его title
+    def authors_names(self):
+        return self.authors.all()
+    def get_absolute_url(self):
+        return "/library/books/%i" % self.id
+    def __unicode__(self):
+        return self.title
+
+'''
+Publisher
+'''
+class Publisher(models.Model):
+    title = models.CharField('Название',max_length=32)
+    address = models.TextField()
+    city = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    website = models.URLField('Адрес сайта')
+    def __unicode__(self):
+        return self.title
---
+++ practice_3/project/project/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'library.views.books'),
+    url(r'^library/$', 'library.views.books'),
+    url(r'^library/books/$', 'library.views.books'),
+    url(r'^library/books/(?P<book_id>\w+)/$', 'library.views.book'),
+    url(r'^library/authors/$', 'library.views.authors'),
+    url(r'^library/authors/(?P<author_id>\w+)/$', 'library.views.author'),
+    #url(r'^log/$', 'pages.views.listing'),
+    #url(r'^log/(?P<dir_name>\w+)/$', 'pages.views.listing'),
+
+    # Examples:
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^project/', include('project.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
